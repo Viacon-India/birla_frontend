94df54d23eca772868715f2e015de9655ee7bf8c
Md Adnan <md.adnan@viaconteam.com>
2024-08-16 14:21:04 +0530

    changes fix

--- app/components/homepage/NewAbout.js
+++ app/components/homepage/NewAbout.js
@@ -1,11 +1,11 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
-import $ from "jquery";
 import { Autoplay } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
+import Link from "next/link";
 
 import icon1 from "../../assets/images/about-icon1.png";
 import icon2 from "../../assets/images/about-icon2.png";
@@ -22,286 +22,126 @@ import img4 from "../../assets/images/about-img-4.jpg";
 import img1H from "../../assets/images/about-icon1.png";
 import GradualSpacing from "@/components/GradualSpacing";
 
+import { FreeMode, Navigation, Thumbs } from "swiper/modules";
+
 export default function NewAbout() {
-
-  useEffect(() => {
-    $(".detail1")
-      .unbind()
-      .mouseenter(function () {
-        $(".detail1Img").fadeIn(600);
-        $(".detail2Img").fadeOut(600);
-        $(".detail3Img").fadeOut(600);
-        $(".detail4Img").fadeOut(600);
-      });
-    $(".detail2")
-      .unbind()
-      .mouseenter(function () {
-        // $('.detail2Img').fadeIn(600);
-        // $('.detail1Img').fadeOut(600);
-        $(".detail1Img").fadeOut(600);
-        $(".detail2Img").fadeIn(600);
-        $(".detail3Img").fadeOut(600);
-        $(".detail4Img").fadeOut(600);
-      });
-    $(".detail3")
-      .unbind()
-      .mouseenter(function () {
-        $(".detail1Img").fadeOut(600);
-        $(".detail2Img").fadeOut(600);
-        $(".detail3Img").fadeIn(600);
-        $(".detail4Img").fadeOut(600);
-      });
-    $(".detail4")
-      .unbind()
-      .mouseenter(function () {
-        $(".detail1Img").fadeOut(600);
-        $(".detail2Img").fadeOut(600);
-        $(".detail3Img").fadeOut(600);
-        $(".detail4Img").fadeIn(600);
-      });
-
-    if (
-      $(".swiper-slide-active")
-        .children()
-        .children(".detail")
-        .hasClass("detail1")
-    ) {
-      $(".detail1Img").fadeIn(600);
-      $(".detail2Img").fadeOut(600);
-      $(".detail3Img").fadeOut(600);
-      $(".detail4Img").fadeOut(600);
-    } else if (
-      $(".swiper-slide-active")
-        .children()
-        .children(".detail")
-        .hasClass("detail2")
-    ) {
-      $(".detail1Img").fadeOut(600);
-      $(".detail2Img").fadeIn(600);
-      $(".detail3Img").fadeOut(600);
-      $(".detail4Img").fadeOut(600);
-    } else if (
-      $(".swiper-slide-active")
-        .children()
-        .children(".detail")
-        .hasClass("detail3")
-    ) {
-      $(".detail1Img").fadeOut(600);
-      $(".detail2Img").fadeOut(600);
-      $(".detail3Img").fadeIn(600);
-      $(".detail4Img").fadeOut(600);
-    } else if (
-      $(".swiper-slide-active")
-        .children()
-        .children(".detail")
-        .hasClass("detail4")
-    ) {
-      $(".detail1Img").fadeOut(600);
-      $(".detail2Img").fadeOut(600);
-      $(".detail3Img").fadeOut(600);
-      $(".detail4Img").fadeIn(600);
-    }
-  }, []);
-
+  const [thumbsSwiper, setThumbsSwiper] = useState(null);
   return (
-    <section className="split-section-wrapper mt-[100px]">
-      <div className="container mx-auto hoverBoxContainer-2">
+    <section className="new-about-slider mt-[100px]">
+      <div className="container mx-auto">
         <span className="section-heading">company brief</span>
         <div className="section-title-wrapper">
           <GradualSpacing className="section-title" text="About Us" />
         </div>
-        <div
-          className="split-section split-section1 w-[100vw]"
-          data-aos="zoom-out-right"
-          data-aos-easing="linear"
-          data-aos-duration="900"
+        <Swiper
+          loop={true}
+          speed={2000}
+          autoplay={{
+            delay: 2000,
+            disableOnInteraction: false,
+          }}
+          spaceBetween={10}
+          navigation={true}
+          thumbs={{ swiper: thumbsSwiper }}
+          modules={[Autoplay]}
+          className="mySwiper2"
         >
-          <div className="split-images">
-            <Image
-              src={img1}
-              alt="card"
-              className="object-cover detailImg detail1Img"
-            />
-            <Image
-              src={img2}
-              alt="card"
-              className="object-cover detailImg detail2Img"
-            />
-            <Image
-              src={img3}
-              alt="card"
-              className="object-cover detailImg detail3Img"
-            />
-            <Image
-              src={img4}
-              alt="card"
-              className="object-cover detailImg detail4Img"
-            />
-          </div>
+          <SwiperSlide>
+            <Image src={img1} className="w-full h-full object-cover" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src={img2} className="w-full h-full object-cover" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src={img3} className="w-full h-full object-cover" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src={img4} className="w-full h-full object-cover" />
+          </SwiperSlide>
+        </Swiper>
 
-          <Swiper
-            loop={true}
-            spaceBetween={10}
-            slidesPerView={2.08}
-            speed={1000}
-            autoplay={{
-              // delay: 6000,
-              pauseOnMouseEnter: true,
-              
-            }}
-            modules={[Autoplay]}
-            className="mySwiper split-details"
-          >
-            <SwiperSlide>
-              <div className="swiper-card-main relative">
-                <div className="detail detail1">
-                  <div className="imageHeading">
-                    <p
-                      className="colorHIcon"
-                      style={{
-                        backgroundImage: `url(${icon1.src})`,
-                        backgroundSize: "cover",
-                        width: "40px",
-                        height: "40px",
-                        display: "block",
-                      }}
-                    ></p>
-                    <p
-                      className="colorIcon"
-                      style={{
-                        backgroundImage: `url(${icon5.src})`,
-                        backgroundSize: "cover",
-                        width: "40px",
-                        height: "40px",
-                        display: "block",
-                      }}
-                    ></p>
-                    <h2 className="about-usp-title">Legacy of Trust</h2>
-                  </div>
-                  <p className="about-usp-detail">
-                    With a rich heritage spanning over three decades, Birla
-                    Tyres has built a reputation for reliability and excellence
-                    in the tyre industry. Over the past three decades, we have
-                    achieved numerous milestones, which is a testament to our
-                    commitment to quality and customer focus. We take pride in
-                    being a trusted partner for drivers across the country.
-                  </p>
-                  <a class="tertiary-button" href="#">
-                    Read more
-                  </a>
-                </div>
-              </div>
-            </SwiperSlide>
-
-            <SwiperSlide>
-              <div className="swiper-card-main relative">
-                <div className="detail detail2">
-                  <div className="imageHeading">
-                    <p
-                      className="colorHIcon"
-                      style={{
-                        backgroundImage: `url(${icon2.src})`,
-                        backgroundSize: "cover",
-                        width: "40px",
-                        height: "40px",
-                        display: "block",
-                      }}
-                    ></p>
-                    <p
-                      className="colorIcon"
-                      style={{
-                        backgroundImage: `url(${icon6.src})`,
-                        backgroundSize: "cover",
-                        width: "40px",
-                        height: "40px",
-                        display: "block",
-                      }}
-                    ></p>
-                    <h2 className="about-usp-title">Vast Range of Products</h2>
-                  </div>
-                  <p className="about-usp-detail">
-                  Our company is proud to offer a comprehensive tyre portfolio that caters to the diverse and unique needs of our customers. With our extensive range of products, including Truck Bus Bias (TBB) tyres, agricultural tyres, and off-the-road tyres, we are equipped to provide solutions for various applications and industries.
-                  </p>
-                  <a class="tertiary-button" href="#">
-                    Read more
-                  </a>
-                </div>
-              </div>
-            </SwiperSlide>
-
-            <SwiperSlide>
-              <div className="swiper-card-main relative">
-                <div className="detail detail3">
-                  <div className="imageHeading">
-                    <p
-                      className="colorHIcon"
-                      style={{
-                        backgroundImage: `url(${icon3.src})`,
-                        backgroundSize: "cover",
-                        width: "40px",
-                        height: "40px",
-                        display: "block",
-                      }}
-                    ></p>
-                    <p
-                      className="colorIcon"
-                      style={{
-                        backgroundImage: `url(${icon7.src})`,
-                        backgroundSize: "cover",
-                        width: "40px",
-                        height: "40px",
-                        display: "block",
-                      }}
-                    ></p>
-                    <h2 className="about-usp-title">Commitment to Quality Excellence</h2>
-                  </div>
-                  <p className="about-usp-detail">
-                  At Birla Tyres, we continuously invest in research and development to stay at the forefront of tyre technology. Our dedicated R&D team works tirelessly to innovate and introduce advanced features that enhance performance, safety and durability.
-                  </p>
-                  <a class="tertiary-button" href="#">
-                    Read more
-                  </a>
-                </div>
-              </div>
-            </SwiperSlide>
-
-            <SwiperSlide>
-              <div className="swiper-card-main relative">
-                <div className="detail detail4">
-                  <div className="imageHeading">
-                    <p
-                      className="colorHIcon"
-                      style={{
-                        backgroundImage: `url(${icon4.src})`,
-                        backgroundSize: "cover",
-                        width: "40px",
-                        height: "40px",
-                        display: "block",
-                      }}
-                    ></p>
-                    <p
-                      className="colorIcon"
-                      style={{
-                        backgroundImage: `url(${icon8.src})`,
-                        backgroundSize: "cover",
-                        width: "40px",
-                        height: "40px",
-                        display: "block",
-                      }}
-                    ></p>
-                    <h2 className="about-usp-title">Customer-Centric Approach</h2>
-                  </div>
-                  <p className="about-usp-detail">
-                  Our customer-centric approach is the cornerstone of our business philosophy. We prioritize understanding and addressing the unique needs and preferences of our customers, ensuring that every interaction and product is tailored to deliver exceptional value.
-                  </p>
-                  <a class="tertiary-button" href="#">
-                    Read more
-                  </a>
-                </div>
-              </div>
-            </SwiperSlide>
-          </Swiper>
-        </div>
+        <Swiper
+          onSwiper={setThumbsSwiper}
+          spaceBetween={10}
+          slidesPerView={2.2}
+          freeMode={true}
+          watchSlidesProgress={true}
+          modules={[FreeMode, Navigation, Thumbs]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <div class="new-about-card">
+              <Image className="colorIcon" src={icon1} />
+              <Image className="colorHIcon" src={icon5} />
+              <h2 className="about-usp-title-2">Legacy of Trust</h2>
+              <p className="about-usp-detail-2">
+                With a rich heritage spanning over three decades, Birla Tyres
+                has built a reputation for reliability and excellence in the
+                tyre industry. Over the past three decades, we have achieved
+                numerous milestones, which is a testament to our commitment to
+                quality and customer focus. We take pride in being a trusted
+                partner for drivers across the country.
+              </p>
+              <Link href="" className="about-button">
+                Read more
+              </Link>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div class="new-about-card">
+              <Image className="colorIcon" src={icon2} />
+              <Image className="colorHIcon" src={icon6} />
+              <h2 className="about-usp-title-2">Vast Range of Products</h2>
+              <p className="about-usp-detail-2">
+                Our company is proud to offer a comprehensive tyre portfolio
+                that caters to the diverse and unique needs of our customers.
+                With our extensive range of products, including Truck Bus Bias
+                (TBB) tyres, agricultural tyres, and off-the-road tyres, we are
+                equipped to provide solutions for various applications and
+                industries.
+              </p>
+              <Link href="" className="about-button">
+                Read more
+              </Link>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div class="new-about-card">
+              <Image className="colorIcon" src={icon3} />
+              <Image className="colorHIcon" src={icon7} />
+              <h2 className="about-usp-title-2">
+                Commitment to Quality Excellence
+              </h2>
+              <p className="about-usp-detail-2">
+                At Birla Tyres, we continuously invest in research and
+                development to stay at the forefront of tyre technology. Our
+                dedicated R&D team works tirelessly to innovate and introduce
+                advanced features that enhance performance, safety and
+                durability.
+              </p>
+              <Link href="" className="about-button">
+                Read more
+              </Link>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div class="new-about-card">
+              <Image className="colorIcon" src={icon4} />
+              <Image className="colorHIcon" src={icon8} />
+              <h2 className="about-usp-title-2">Customer-Centric Approach</h2>
+              <p className="about-usp-detail-2">
+                Our customer-centric approach is the cornerstone of our business
+                philosophy. We prioritize understanding and addressing the
+                unique needs and preferences of our customers, ensuring that
+                every interaction and product is tailored to deliver exceptional
+                value.
+              </p>
+              <Link href="" className="about-button">
+                Read more
+              </Link>
+            </div>
+          </SwiperSlide>
+        </Swiper>
       </div>
     </section>
   );
