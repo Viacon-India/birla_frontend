b352242eb5dc228822e71676e6c615a50e3e4432
Adnan <mdadnan0425@gmail.com>
2024-08-28 11:41:41 +0530

    page added

--- app/leadership/section/Next-Chapter.js
+++ app/leadership/section/Next-Chapter.js
@@ -10,9 +10,9 @@ import BGTiger from "../../assets/images/tiger-mask3.png";
 export default function NewAbout() {
   return (
     <section className="mt-10 md:mt-[60px] 2xl:mt-[100px] overflow-hidden flex">
-            <Image src={NextChapter} className="absolute left-0 h-[200px] w-[60%]" />
+            <Image src={NextChapter} alt="bottom-img" className="absolute left-0 h-[200px] w-[60%]" />
         <div className="container mx-auto relative h-[200px] flex items-end justify-end">
-            <Image src={BGTiger} className="absolute right-[20%]" />
+            <Image src={BGTiger} alt="bottom-img" className="absolute right-[20%]" />
             <p className="flex font-oak-sans font-bold text-2xl text-[#1A1D21] gap-1 pb-4">
                 Next Page:
                 <Link className="text-[#F5811E] font-medium flex items-center gap-1" href="/users/product">
--- app/who-we-are/page.jsx
+++ app/who-we-are/page.jsx
@@ -1,13 +1,47 @@
 import React from "react";
 import Navbar from "../components/navbar/navbar";
 import Footer from "../components/footer/footer";
+import Float from "../components/float/float";
+import Banner from "../assets/images/who1.jpg";
+import Link from "next/link";
+import Image from "next/image";
+import GradualSpacing from "@/components/GradualSpacing";
 
 
 export default function WhoWeAre() {
   return (
     <>
       <Navbar />
-      <section>WhoWeAre</section>
+      <section className="top-banner-sec">
+      <div className="relative">
+      <Float />
+      <div className="relative">
+        <div className="w-full h-[83vh] flex items-end 2xl:pb-[60px]">
+          <div class="container mx-auto flex flex-col justify-around 2xl:justify-between h-[80%]">
+            <div class="flex flex-col gap-5 relative z-10">
+              <Link href="" className="primary-btn-1">
+                Find a Tyre
+              </Link>
+              <Link href="" className="primary-btn-2">
+                Find a Dealer
+              </Link>
+            </div>
+            <div className="swiper-card relative z-10">
+              <GradualSpacing
+                className="leadership-hero-sec-heading font-bold text-[56px] text-[#1A1D21] bg-white"
+                text="WHO WE ARE" padding="5"
+              />
+            </div>
+          </div>
+        </div>
+        <Image
+          src={Banner}
+          alt="Hero-Banner"
+          className="absolute top-0 w-full h-full object-cover"
+        />
+      </div>
+    </div>
+      </section>
       <Footer />
     </>
   );
