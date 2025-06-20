f7d7f3959027bc7e862852005c82bc5ec939372f
Dibyendu-Viacon <dibyendu.ghosh@viaconteam.com>
2025-06-20 19:36:18 +0530

    Response error bugs fixes done

--- app/api/submit-form/route.js
+++ app/api/submit-form/route.js
@@ -1,6 +1,6 @@
-export default async function handler(req, res) {
-  const { captcha } = req.body;
-  
+export async function POST(request) {
+  const { captcha } = await request.json();
+
   const response = await fetch(
     `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${captcha}`,
     { method: "POST" }
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
   const data = await response.json();
 
   if (!data.success) {
-    return res.status(400).json({ error: "CAPTCHA verification failed" });
+    return Response.json({ error: "CAPTCHA verification failed" }, { status: 400 });
   }
-  res.status(200).json({ success: true });
+  return Response.json({ success: true }, { status: 200 });
 }
