61729700d3a6259a4b38494e8ed20ce77afaa322
mdadnan2023 <md.adnan@viaconteam.com>
2025-06-23 19:00:58 +0530

    google tag added

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,23 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google tag (gtag.js) */}
+           <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-6HN4GVNJ9Y"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="ga-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-6HN4GVNJ9Y');
+            `,
+          }}
+        />
         {/* GTM Script */}
         <Script
           id="gtm-script"
