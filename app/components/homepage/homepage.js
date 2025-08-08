bc81bd9313476e547dbca58e353b00b33dc61d39
mdadnan2023 <md.adnan@viaconteam.com>
2025-08-08 18:59:06 +0530

    cwv lcp score reduce to 3.7

--- app/components/homepage/homepage.js
+++ app/components/homepage/homepage.js
@@ -1,37 +1,61 @@
 import React, { useEffect, useState } from "react";
 import { getStrapiMedia } from "@/lib/utils";
- 
+
 import { SingleTypeSeo, Popup } from "@/app/components/pageCommon/pageCommon";
- 
-import Hero from "./Hero"
+
+import Hero from "./Hero";
 import Innovation from "./innovation";
 import Product from "./product";
 import Dealer from "./dealer";
 import Sustainability from "./sustainability";
-import AboutUs from "./about"
+import AboutUs from "./about";
 import Media from "./media";
-import Faq from "./faq"
- 
+// import dynamic from "next/dynamic";
+import Faq from "./faq";
+// const Faq = dynamic(() => import("./faq"), { ssr: false });
+
 export default function Homepage() {
   const [pageData, setPageData] = useState([]);
+
   useEffect(() => {
-    fetch(getStrapiMedia("/api/home")).then((res) => res.json()).then((page) => {
-      setPageData(page);
-    });
+    fetch(getStrapiMedia("/api/home"))
+      .then((res) => res.json())
+      .then((page) => {
+        setPageData(page);
+      });
   }, []);
   return (
     <>
-      <SingleTypeSeo pageData={pageData} page='website' />
+      <SingleTypeSeo pageData={pageData} page="website" />
       <Hero Data={pageData.data?.attributes?.categories}/>
       <Popup />
-      <Innovation Heading={pageData.data?.attributes?.section1_heading} Title={pageData.data?.attributes?.section1_title} Data={pageData.data?.attributes?.categories}/>
-      <Product Heading={pageData.data?.attributes?.section2_heading} Title={pageData.data?.attributes?.section2_title} Data={pageData.data?.attributes?.categories}/>
+      <Innovation
+        Heading={pageData.data?.attributes?.section1_heading}
+        Title={pageData.data?.attributes?.section1_title}
+        Data={pageData.data?.attributes?.categories}
+      />
+      <Product
+        Heading={pageData.data?.attributes?.section2_heading}
+        Title={pageData.data?.attributes?.section2_title}
+        Data={pageData.data?.attributes?.categories}
+      />
       {/* <Dealer Heading={pageData.data?.attributes?.section3_heading} Title={pageData.data?.attributes?.section3_title}/> */}
-      <Sustainability Heading={pageData.data?.attributes?.section4_heading} Title={pageData.data?.attributes?.section4_title} Description={pageData.data?.attributes?.section4_description} Video={pageData.data?.attributes?.section4_video}/>
-      <AboutUs Data={pageData.data?.attributes?.section5}/>
-      <Media Heading={pageData.data?.attributes?.media_heading} Title={pageData.data?.attributes?.media_title}/>
-      <Faq Heading={pageData.data?.attributes?.blog_heading} Title={pageData.data?.attributes?.blog_title} Data={pageData.data?.attributes?.faq}/>
- 
+      <Sustainability
+        Heading={pageData.data?.attributes?.section4_heading}
+        Title={pageData.data?.attributes?.section4_title}
+        Description={pageData.data?.attributes?.section4_description}
+        Video={pageData.data?.attributes?.section4_video}
+      />
+      <AboutUs Data={pageData.data?.attributes?.section5} />
+      <Media
+        Heading={pageData.data?.attributes?.media_heading}
+        Title={pageData.data?.attributes?.media_title}
+      />
+      <Faq
+        Heading={pageData.data?.attributes?.blog_heading}
+        Title={pageData.data?.attributes?.blog_title}
+        Data={pageData.data?.attributes?.faq}
+      />
     </>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,26 @@
 "use client"
 
-import { useState, useEffect } from "react";
+import { useState,Suspense, useEffect } from "react";
 import Link from 'next/link';
 import Navbar from "./components/navbar/navbar";
 import Footer from "./components/footer/footer";
 import Homepage from "./components/homepage/homepage";
-// import Cursor from "../app/components/Cursor"
+import Image from "next/image";
+import loadGif from "./assets/images/loading.gif";
+
 
 
 
 export default function Home() {
-  const [isLoading, setLoading] = useState(true);
+  // const [isLoading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 2000);
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     setLoading(false);
+  //   }, 2000);
 
-    return () => clearTimeout(timer);
-  }, []);
+  //   return () => clearTimeout(timer);
+  // }, []);
 
   // if (isLoading) return <Loader />;
 
@@ -26,7 +28,15 @@ export default function Home() {
     <>
       {/* <Cursor /> */}
       <Navbar />
+            <Suspense
+              fallback={
+                <div>
+                  <Image className="w-[40px] h-[40px]" src={loadGif} alt="loadigif" />
+                </div>
+              }
+            >
       <Homepage />
+      </Suspense>
       <Footer />
     </>
   );
