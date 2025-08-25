f5058b09c721fa6b96668c758f9cceec63d27f61
mdadnan2023 <md.adnan@viaconteam.com>
2025-08-25 18:24:08 +0530

    seo fixes

--- app/components/homepage/media.js
+++ app/components/homepage/media.js
@@ -1,4 +1,5 @@
-import React from "react";
+"use client"
+import React, { use } from "react";
 import { useState, useEffect } from "react";
 import { getStrapiMedia } from "@/lib/utils";
 import Image from "next/image";
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+// "use client"
 
 import { useState,Suspense, useEffect } from "react";
 import Link from 'next/link';
@@ -7,11 +7,61 @@ import Footer from "./components/footer/footer";
 import Homepage from "./components/homepage/homepage";
 import Image from "next/image";
 import loadGif from "./assets/images/loading.gif";
+import { getStrapiMedia } from "@/lib/utils";
 
+export async function generateMetadata() {
+  const apiUrl = getStrapiMedia("/api/home");
+  if (!apiUrl) {
+    throw new Error("API URL is null");
+  }
+  const res = await fetch(apiUrl);
+  const pageData = await res.json();
 
+  const attrs = pageData?.data?.attributes || {};
 
+  return {
+    title: attrs.seo_title || attrs.title || "Default Title",
+    description: attrs.seo_description || "Default Description",
+    keywords: attrs.seo_keywords || "",
+    openGraph: {
+      title: attrs.seo_title,
+      description: attrs.seo_description,
+      url: process.env.DOMAIN_NAME + (attrs.permalink || ""),
+      siteName: "Birla Tyres",
+      images: attrs.seo_image?.data
+        ? [
+            {
+              url: getStrapiMedia(attrs.seo_image.data.attributes?.url),
+              width: attrs.seo_image.data.attributes?.width,
+              height: attrs.seo_image.data.attributes?.height,
+              type: attrs.seo_image.data.attributes?.mime,
+            },
+          ]
+        : [],
+      type: "website",
+    },
+    twitter: {
+      card: attrs.seo_title,
+      title: attrs.seo_title,
+      description: attrs.seo_description,
+      images: attrs.seo_image?.data
+        ? [getStrapiMedia(attrs.seo_image.data.attributes?.url)]
+        : [],
+    },
+    alternates: {
+      canonical: process.env.DOMAIN_NAME + (attrs.permalink || ""),
+    },
+  };
+}
 
-export default function Home() {
+export default async function Home() {
+  const apiUrl = getStrapiMedia("/api/home");
+  if (!apiUrl) {
+    throw new Error("API URL is null");
+  }
+  const res = await fetch(apiUrl);
+  const pageData = await res.json();
+
   // const [isLoading, setLoading] = useState(true);
 
   // useEffect(() => {
@@ -35,7 +85,7 @@ export default function Home() {
                 </div>
               }
             >
-      <Homepage />
+      <Homepage pageData={pageData} />
       </Suspense>
       <Footer />
     </>
