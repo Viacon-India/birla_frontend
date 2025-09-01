3844c3fff831a1368b218a015aadf4c8710b0e9f
mdadnan2023 <md.adnan@viaconteam.com>
2025-09-01 19:13:26 +0530

    fix next config

--- next.config.js
+++ next.config.js
@@ -15,6 +15,18 @@ const nextConfig = {
     env: {
         DOMAIN_NAME: process.env.DOMAIN_NAME,
     },
+ 
+    // ✅ Redirect www → non-www
+    async redirects() {
+        return [
+            {
+                source: '/:path*',
+                has: [{ type: 'host', value: 'www.birlatyre.com' }],
+                destination: 'https://birlatyre.com/:path*',
+                permanent: true,
+            },
+        ]
+    },
 };
-
-module.exports = nextConfig
+ 
+module.exports = nextConfig;
